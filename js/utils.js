58d5ba64cb700390daf2b6ed26cbdef8168bda5b
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-11-25 19:39:56 -0300

    feat: export to index.js | options and restart features

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,10 @@
 const menu = document.getElementById('menu');
 const game = document.getElementById('game');
 
+export const options = document.querySelectorAll('.frame .option');
 export const play = document.getElementById('play');
 export const goBack = document.getElementById('back');
+export const restart = document.getElementById('reset');
 
 play.addEventListener('click', () => {
   menu.classList.add('disable');
