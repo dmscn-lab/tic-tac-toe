989d6156fcb4e84df76d1b60cd02fd5b3ef7d05d
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-06 17:45:17 -0300

    feat: export events to show and modify dialog

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,4 @@
 export const options = document.querySelectorAll('.frame .option');
+export const status = document.querySelector('.status');
+export const showStatus = () => status.showModal();
+export const closeStatus = () => status.close();
