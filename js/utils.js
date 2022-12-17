1887299c4e1e958b3e7be8eeac3d1822b3adc9ed
wesleydamasceno <wesleydamasceno.co@gmail.com>
2022-12-17 18:04:30 -0300

    refactor: modals exports

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,4 @@
 export const options = document.querySelectorAll('.frame .option');
 export const status = document.querySelector('.status');
-export const showStatus = () => status.showModal();
-export const closeStatus = () => status.close();
+export const showModal = () => status.showModal();
+export const closeModal = () => status.close();
